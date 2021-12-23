8a913d082de9d5a84e5b0b1279a58409d4446da9
Georgi Petkov <petkovgogo@gmail.com>
2021-12-23 14:37:26 +0200

    Added basic GPIO class

---
+++ src/HydroponicGarden.cpp
@@ -0,0 +1,18 @@
+//============================================================================
+// Name        : HydroponicGarden.cpp
+// Author      : georgip
+// Version     :
+// Copyright   : 
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+
+extern int gpioTest ();
+
+int main ()
+{
+    gpioTest ();
+
+    return 0;
+}
