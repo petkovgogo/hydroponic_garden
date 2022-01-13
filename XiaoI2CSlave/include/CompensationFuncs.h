5f502f845c2baa587d5cf529220d463b8c07cba8
Georgi Petkov <petkovgogo@gmail.com>
2022-01-13 13:29:59 +0200

    Added compensation function for TDS sensor

---
+++ XiaoI2CSlave/include/CompensationFuncs.h
@@ -0,0 +1,12 @@
+#ifndef COMP_FUNCS_H_
+#define COMP_FUNCS_H_
+
+#include <cstdint>
+
+namespace CompensationFuncs
+{
+    float compensateTDSData(uint16_t rawValue);
+} // namespace CompensationFuncs
+
+
+#endif // COMP_FUNCS_H_
---
+++ XiaoI2CSlave/src/compFuncs.cpp
@@ -0,0 +1,21 @@
+#include "CompensationFuncs.h"
+
+#include <cmath>
+
+static constexpr float VREF = 3.3;                // analog reference voltage(Volt) of the ADC
+static constexpr float ADC_RES = 4095.0;          // 12bit resolution
+static constexpr float ADC_COEF = VREF / ADC_RES; // mapping coefficient
+
+float CompensationFuncs::compensateTDSData(uint16_t rawValue)
+{
+    float temperature = 25; // TODO get actual reading from BME280
+
+    float averageVoltage = rawValue * ADC_COEF;                           // read the analog value more stable by the median filtering algorithm, and convert to voltage value
+    float compensationCoefficient = 1.0 + 0.02 * (temperature - 25.0);    // temperature compensation formula: fFinalResult(25^C) = fFinalResult(current)/(1.0+0.02*(fTP-25.0));
+    float compensationVolatge = averageVoltage / compensationCoefficient; // temperature compensation
+
+    return (133.42 * pow(compensationVolatge, 3) -
+            255.86 * pow(compensationVolatge, 2) +
+            857.39 * compensationVolatge) *
+           0.5; // convert voltage value to tds value
+}
