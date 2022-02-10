e9e8a53c6c081e23863ab15c01e6fa65179fa8be
Georgi Petkov <petkovgogo@gmail.com>
2022-02-10 16:28:33 +0200

    Added CGI JSON response for liquid level sensor

---
+++ NginX/cgi-src/cgi-liquid-level.cpp
@@ -0,0 +1,18 @@
+#include "GPIO.h"
+
+#include <iostream>
+
+int main()
+{
+    constexpr uint8_t SENSOR_PIN = 4;
+
+    GPIO liquidLevel(SENSOR_PIN);
+    liquidLevel.setDirection(GPIO_DIRECTION::INPUT);
+
+    std::cout << "Content-Type: application/json\n\n";
+    std::cout << "{\n";
+    std::cout << "    \"state:\" \"" << liquidLevel.getState() << "\"\n";
+    std::cout << "}" << std::endl;
+
+    return 0;
+}
