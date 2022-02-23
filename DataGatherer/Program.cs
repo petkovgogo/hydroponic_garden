82917dca83fe988d77744a6c145eba16770ec153
Georgi Petkov <petkovgogo@gmail.com>
2022-02-23 15:26:34 +0200

    Added timed service

--- DataGatherer/Program.cs
+++ DataGatherer/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        IHost host = new HostBuilder()
+            .ConfigureServices(services => 
+            {
+                services.AddHostedService<TimedHostedService>();
+                services.AddHttpClient();
+                services.AddScoped<DataService>();
+            })
+            .ConfigureLogging((hostContext, configureLogging) => 
+            {
+                configureLogging.AddConsole();
+                configureLogging.AddDebug();
+                configureLogging.SetMinimumLevel(LogLevel.Debug);
+            })
+            .Build();
+            
+        await host.RunAsync();
+    }
+}
